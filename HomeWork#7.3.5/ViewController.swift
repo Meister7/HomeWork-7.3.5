4d38e3c7772c5604c1f7a1193ec5c48ae4f5781c
Meister7 <emirkarmyshev7@gmail.com>
2022-01-29 13:29:42 +0600

    Initial Commit

---
+++ HomeWork#7.3.5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork#7.3.5
+//
+//  Created by Эмир Кармышев on 29/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HomeWork#7.3.5UITests/HomeWork_7_3_5UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HomeWork_7_3_5UITestsLaunchTests.swift
+//  HomeWork#7.3.5UITests
+//
+//  Created by Эмир Кармышев on 29/1/22.
+//
+
+import XCTest
+
+class HomeWork_7_3_5UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
